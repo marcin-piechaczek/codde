8242a6f1e70ed59cad27bcbd4b6e94c6423c4cf9
Marcin Piechaczek <marcin@piechaczek.dev>
2023-05-25 19:52:45 +0200

    fix: fixed loading state

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,6 @@
 import { Container } from '@/components/Container';
 import { Projects } from '@/components/Projects';
-import { SkeletonGrid } from '@/components/SkeletonGrid';
 import { Wrapper } from '@/components/Wrapper';
-import { Suspense } from 'react';
 
 interface SearchProps {
   searchParams?: { [key: string]: string | string[] | undefined };
@@ -15,11 +13,9 @@ export default async function SearchPage({ searchParams }: SearchProps) {
   return (
     <Container>
       <Wrapper>
-        <Suspense fallback={<SkeletonGrid elements={4} />}>
-          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-          {/* @ts-expect-error /> */}
-          {searchedPackage && <Projects page={page} search={searchedPackage} />}
-        </Suspense>
+        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+        {/* @ts-expect-error /> */}
+        {searchedPackage && <Projects page={page} search={searchedPackage} />}
       </Wrapper>
     </Container>
   );
